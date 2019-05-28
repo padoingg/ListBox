57417cb378197089d870e757be03878c3d7a08f7
Namhee Kang <padoingg@gmail.com>
2019-05-28 15:15:02 +0900

    #2 항목을 추가했을 때 생기는 이벤트 코드 구현

--- ListBox/Form1.Designer.cs
+++ ListBox/Form1.Designer.cs
@@ -42,6 +42,7 @@
             this.lbView.Name = "lbView";
             this.lbView.Size = new System.Drawing.Size(167, 160);
             this.lbView.TabIndex = 0;
+            this.lbView.SelectedIndexChanged += new System.EventHandler(this.LbView_SelectedIndexChanged);
             // 
             // lblResult
             // 
@@ -67,6 +68,8 @@
             this.btnAdd.TabIndex = 3;
             this.btnAdd.Text = "추가";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            this.btnAdd.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.LbView_KeyPress);
             // 
             // Form1
             // 
@@ -81,6 +84,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "리스트 추가";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
